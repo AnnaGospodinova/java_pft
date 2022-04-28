8d165238814ffb4b966ed142bc36aab4f648ebba
AnnaGospodinova <75027451+AnnaGospodinova@users.noreply.github.com>
2022-04-28 15:37:30 +0300

    Задание №2: расчёт расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,22 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    hello("world");
+    hello("user");
+    hello("Anna");
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(3, 5);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p = new Point(-5, 3.1, 3, -2.5);
+    System.out.println("Расстояние между точкой (" + p.x1 + ", " + p.y1 + ") и (" + p.x2 + ", " + p.y2 + ") = " + p.distance());
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody);
   }
 
 }
